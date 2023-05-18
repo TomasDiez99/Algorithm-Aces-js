72a45ad075f2a182bd99eebf079ec49c3dfe20dd
AxelFontana <axelfontana01@gmail.com>
2023-05-18 18:38:21 -0300

    Page footer

---
+++ la-gloria-store/src/HomeComponents/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => {
+    const handleGithubClick = () => {
+        window.open('https://github.com/TomasDiez99/Algorithm-Aces-js', '_blank');
+    };
+
+    return (
+        <footer className="bg-dark text-light">
+            <div className="container py-2">
+                <div className="row justify-content-center text-center">
+                    <div className="col-auto">
+                        <i className="fab fa-github" onClick={handleGithubClick}></i>
+                    </div>
+                    <div className="col-auto">
+                        <i className="fab fa-react"></i>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
