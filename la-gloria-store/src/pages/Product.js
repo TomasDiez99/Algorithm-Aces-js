549149aaafbc2e0cacfd6840e13edae4418e1e29
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-05-21 11:53:17 -0300

    basic api and product list show

--- la-gloria-store/src/pages/Product.js
+++ la-gloria-store/src/pages/Product.js
@@ -1,20 +1,46 @@
 import logo from "../logo.svg";
 import "../App.css";
+import React, { useEffect, useState } from "react";
 
 function Product() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://la-gloria-store-algorithm-aces.vercel.app/rest/products"
+        );
+        const json = await response.json();
+        setProducts(json.data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [products]); // Agrega "products" como dependencia
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>PRODUCTTEST</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Products</h1>
+          {products.length > 0 ? (
+            <ul>
+              {products.map((product) => (
+                <li key={product.id}>
+                  <h3>{product.name}</h3>
+                  <p>Price: {product.price}</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Loading products...</p>
+          )}
+        </div>
       </header>
     </div>
   );
