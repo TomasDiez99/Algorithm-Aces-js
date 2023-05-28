605cad694ff44c9acf8ea7fbe310be53d71b127c
Axel Fontana <axelfontana09@gmail.com>
2023-05-28 18:44:08 -0300

    Carrousel eliminated from Home

--- la-gloria-store/src/pages/Home.js
+++ la-gloria-store/src/pages/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ProductTable from '../HomeComponents/ProductTable/ProductTable';
 import '../App.css';
-import Carousel from "../HomeComponents/Carousel";
+
 
 
 function Home() {
 
     return (
         <div>
-            <Carousel/>
             <ProductTable/>
         </div>
     );
