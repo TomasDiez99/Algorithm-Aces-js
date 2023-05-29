b42d6d30b788efd03f1e42e3b6a64d401e4ae57b
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-05-29 16:25:04 -0300

    product page link in product cards

--- la-gloria-store/src/HomeComponents/ProductTable/ProductCard.js
+++ la-gloria-store/src/HomeComponents/ProductTable/ProductCard.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -18,6 +19,30 @@ function ProductCard(props) {
             </div>
         </div>
     );
+=======
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductCard(props) {
+  const { id, name, image, price } = props;
+
+  return (
+    <div className="card" style={{ width: "14rem" }}>
+      <img
+        src="https://2.bp.blogspot.com/-_Tx3nbVr5mM/U7ctz8a3LDI/AAAAAAAAAkM/YDDEPDIHjVE/s1600/escudoo.png"
+        className="card-img-top"
+        alt={name}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">${price}</p>
+        <Link to={`/product/${id}`} className="btn btn-primary">
+          See details
+        </Link>
+      </div>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default ProductCard;
