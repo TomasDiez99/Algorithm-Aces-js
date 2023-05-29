d5375fd0ad00cd9f700c8123ed22831bc3ba1985
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-05-29 17:34:00 -0300

    added style and cart managing buttons

--- la-gloria-store/src/pages/Product.js
+++ la-gloria-store/src/pages/Product.js
@@ -12,6 +12,7 @@ import {
 
 function Product() {
   const [product, setProduct] = useState(null);
+  const [quantity, setQuantity] = useState(1);
   const params = useParams();
   const productId = params.productId;
 
@@ -35,51 +36,81 @@ function Product() {
     fetchData(productId);
   }, [productId]);
 
+  const handleProductAmount = (newAmount) => {
+    if (quantity >= 1) {
+      setQuantity(newAmount);
+    }
+  };
+
+  const handleAddToCart = () => {
+    console.log(`Adding ${quantity} ${product.name} to cart...`);
+  };
+
   return (
     <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          {/* Carrusel */}
-          <MDBCarousel activeItem={1} length={3} showControls showIndicators>
-            <MDBCarouselInner>
-              <MDBCarouselItem itemId="1">
-                <img
-                  className="d-block w-100"
-                  src="imagen1.jpg"
-                  alt="Imagen 1"
-                />
-              </MDBCarouselItem>
-              <MDBCarouselItem itemId="2">
-                <img
-                  className="d-block w-100"
-                  src="imagen2.jpg"
-                  alt="Imagen 2"
-                />
-              </MDBCarouselItem>
-              <MDBCarouselItem itemId="3">
-                <img
-                  className="d-block w-100"
-                  src="imagen3.jpg"
-                  alt="Imagen 3"
-                />
-              </MDBCarouselItem>
-            </MDBCarouselInner>
-          </MDBCarousel>
-        </MDBCol>
-        <MDBCol md="6">
-          {/* Texto de prueba */}
-          <div>
-            <h1>Product</h1>
-            {product ? (
-              <div>
-                <h3>Name: {product.name}</h3>
-                <p>Price: {product.price}</p>
-              </div>
-            ) : (
-              <p>Loading product...</p>
-            )}
-          </div>
-        </MDBCol>
+      <MDBRow className="product-container">
+        <div className="carousel-container">
+          <MDBCol md="6">
+            {/* Carrusel */}
+            <div className="carousel-wrapper">
+              <MDBCarousel
+                activeItem={1}
+                length={1}
+                showControls
+                showIndicators
+              >
+                <MDBCarouselInner>
+                  <MDBCarouselItem itemId="1">
+                    <img
+                      className="carousel-image"
+                      src="https://2.bp.blogspot.com/-_Tx3nbVr5mM/U7ctz8a3LDI/AAAAAAAAAkM/YDDEPDIHjVE/s1600/escudoo.png"
+                      alt="Imagen 1"
+                    />
+                  </MDBCarouselItem>
+                </MDBCarouselInner>
+              </MDBCarousel>
+            </div>
+          </MDBCol>
+        </div>
+        <div className="product-data-container">
+          <MDBCol md="6">
+            <div className="product-data-content">
+              <h1>Product info</h1>
+              {product ? (
+                <div>
+                  <h3><strong>{product.name}</strong></h3>
+                  <p>Price: ${product.price}</p>
+                  <p>Category: {product.category.name}</p>
+                  <p>Brand: {product.brand.name}</p>
+                  <div className="quantity-control">
+                    <button
+                      className="btn btn-primary btn-sm"
+                      onClick={() => handleProductAmount(quantity - 1)}
+                      disabled={quantity === 1}
+                    >
+                      -
+                    </button>
+                    <span className="quantity">{quantity}</span>
+                    <button
+                      className="btn btn-primary btn-sm"
+                      onClick={() => handleProductAmount(quantity + 1)}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <button
+                    className="btn btn-outline-success btn-lg"
+                    onClick={handleAddToCart}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              ) : (
+                <p>Loading product...</p>
+              )}
+            </div>
+          </MDBCol>
+        </div>
       </MDBRow>
     </MDBContainer>
   );
