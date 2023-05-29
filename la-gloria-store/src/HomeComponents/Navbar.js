51fe943842439a4258b07377334040f93953dee7
Axel Fontana <axelfontana09@gmail.com>
2023-05-28 21:20:29 -0300

    Deleted Navbar Home button y search bar

--- la-gloria-store/src/HomeComponents/Navbar.js
+++ la-gloria-store/src/HomeComponents/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import DropDownButton from './DropDownButton';
+
 
 const Navbar = () => {
     return (
@@ -13,16 +13,6 @@ const Navbar = () => {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="my-app/src/Components/features/navbar#">Home</a>
-                        </li>
-                    </ul>
-                    <DropDownButton/>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </nav>
