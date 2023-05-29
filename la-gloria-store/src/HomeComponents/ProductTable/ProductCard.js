23ab091920bf8c807b7f485b639d35c376c462b8
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-05-29 16:27:50 -0300

    bug fix

--- la-gloria-store/src/HomeComponents/ProductTable/ProductCard.js
+++ la-gloria-store/src/HomeComponents/ProductTable/ProductCard.js
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-
-function ProductCard(props) {
-    const { id, name, image, price } = props;
-
-    return (
-        <div className="card" style={{ width: '14rem' }}>
-            <img src='https://2.bp.blogspot.com/-_Tx3nbVr5mM/U7ctz8a3LDI/AAAAAAAAAkM/YDDEPDIHjVE/s1600/escudoo.png' className="card-img-top" alt={name} />
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <p className="card-text">{price}</p>
-                <Link to={`/product/${id}`} className="btn btn-primary">
-    See details
-</Link>
-
-            </div>
-        </div>
-    );
-=======
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -42,7 +20,6 @@ function ProductCard(props) {
       </div>
     </div>
   );
->>>>>>> Stashed changes
 }
 
 export default ProductCard;
