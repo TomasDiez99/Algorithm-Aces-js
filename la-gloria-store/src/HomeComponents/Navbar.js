77e06742307c19f7b39945936981e0d1d9adf1be
Axel Fontana <axelfontana09@gmail.com>
2023-05-28 21:25:40 -0300

    Added Link to home page in the logo

--- la-gloria-store/src/HomeComponents/Navbar.js
+++ la-gloria-store/src/HomeComponents/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="my-app/src/Components/features/navbar#">
+                <Link to="/" className="navbar-brand">
                     <img src="https://2.bp.blogspot.com/-_Tx3nbVr5mM/U7ctz8a3LDI/AAAAAAAAAkM/YDDEPDIHjVE/s1600/escudoo.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top"/>{' '}
-                      La Gloria Store
-                </a>
+                    La Gloria Store
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
