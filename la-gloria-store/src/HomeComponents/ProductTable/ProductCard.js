9b5dbbe790aec5b549d198bfc06ddc5b5d36b6a1
AxelFontana <axelfontana01@gmail.com>
2023-05-29 22:42:29 -0300

    See details button centered

--- la-gloria-store/src/HomeComponents/ProductTable/ProductCard.js
+++ la-gloria-store/src/HomeComponents/ProductTable/ProductCard.js
@@ -1,25 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function ProductCard(props) {
-  const { id, name, image, price } = props;
+    const {id, name, image, price} = props;
 
-  return (
-    <div className="card" style={{ width: "14rem" }}>
-      <img
-        src="https://2.bp.blogspot.com/-_Tx3nbVr5mM/U7ctz8a3LDI/AAAAAAAAAkM/YDDEPDIHjVE/s1600/escudoo.png"
-        className="card-img-top"
-        alt={name}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <p className="card-text">${price}</p>
-        <Link to={`/product/${id}`} className="btn btn-primary">
-          See details
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className="card" style={{width: "14rem"}}>
+            <img
+                src="https://2.bp.blogspot.com/-_Tx3nbVr5mM/U7ctz8a3LDI/AAAAAAAAAkM/YDDEPDIHjVE/s1600/escudoo.png"
+                className="card-img-top"
+                alt={name}
+            />
+            <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text">${price}</p>
+                <div className="text-center">
+                    <Link to={`/product/${id}`} className="btn btn-primary">
+                        See details
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ProductCard;
