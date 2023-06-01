02095b46288d5d9fc901a2ef21704d30ab3ff0f3
AxelFontana <axelfontana01@gmail.com>
2023-06-01 17:03:39 -0300

    Now if a product is disabled it shows Product Unavailable

--- la-gloria-store/src/HomeComponents/ProductTable/ProductCard.js
+++ la-gloria-store/src/HomeComponents/ProductTable/ProductCard.js
@@ -2,26 +2,28 @@ import { Link } from "react-router-dom";
 import "../home.css";
 
 function ProductCard(props) {
-  const { id, name, image, price } = props;
+    const { id, name, image, price, enable } = props;
 
-  return (
-    <div className="card">
-      <img
-        src={image}
-        className="card-img-top"
-        alt={name}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <p className="card-text">${price}</p>
-        <div className="text-center">
-          <Link to={`/product/${id}`} className="btn btn-primary">
-            See details
-          </Link>
+    return (
+        <div className="card">
+            <img src={image} className="card-img-top" alt={name} />
+            <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text">${price}</p>
+                <div className="text-center">
+                    {enable ? (
+                        <Link to={`/product/${id}`} className="btn btn-primary">
+                            See details
+                        </Link>
+                    ) : (
+                        <button className="btn btn-primary" disabled>
+                            Product Unavailable
+                        </button>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ProductCard;
