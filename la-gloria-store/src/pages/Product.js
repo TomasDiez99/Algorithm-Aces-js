16eed16d9da661e4fe27d8352fc3800db45ac67a
AxelFontana <axelfontana01@gmail.com>
2023-06-04 14:22:03 -0300

    Added the fuction checkProductUnavailable

--- la-gloria-store/src/pages/Product.js
+++ la-gloria-store/src/pages/Product.js
@@ -1,55 +1,64 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import ProductCarrousel from "../components/ProductPageComponents/ProductCarrousel";
 import ProductPageContent from "../components/ProductPageComponents/ProductPageContent";
-import { useParams } from "react-router-dom";
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import {useParams} from "react-router-dom";
+import {MDBContainer, MDBRow, MDBCol} from "mdbreact";
 import "../styles/product-page.css";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 function Product(prop) {
-  const { addOrderDetails } = prop;
-  const [product, setProduct] = useState(null);
+    const {addOrderDetails} = prop;
+    const [product, setProduct] = useState(null);
 
-  const params = useParams();
-  const productId = params.productId;
-  const navigate = useNavigate();
+    const params = useParams();
+    const productId = params.productId;
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchData = async (productId) => {
-      try {
-        const response = await fetch(
-          `https://la-gloria-store-algorithm-aces.vercel.app/rest/products/id/${productId}`
-        );
-        if (response.ok) {
-          const json = await response.json();
-          setProduct(json.data);
-        } else {
-          navigate("/error");
+    useEffect(() => {
+        const fetchData = async (productId) => {
+            try {
+                const response = await fetch(
+                    `https://la-gloria-store-algorithm-aces.vercel.app/rest/products/id/${productId}`
+                );
+                if (response.ok) {
+                    const json = await response.json();
+                    setProduct(json.data);
+                    const localProduct = json.data
+                    checkProductUnavailable(localProduct);
+                } else {
+                    navigate("/error");
+                }
+            } catch (error) {
+                navigate("/error");
+            }
+        };
+
+        fetchData(productId);
+    }, [productId]);
+
+    const checkProductUnavailable = (product) => {
+        if (product === 0 || product.enable === false) {
+            navigate("/error");
         }
-      } catch (error) {
-          navigate("/error");
-      }
     };
 
-    fetchData(productId);
-  }, [productId]);
-
-  return (
-    <div className="product-page-container">
-      <MDBContainer>
-        <MDBRow>
-          <MDBCol md="6">
-            <ProductCarrousel product={product} />
-          </MDBCol>
-          <MDBCol md="6">
-            <ProductPageContent
-              product={product}
-              addOrderDetails={addOrderDetails}
-            />
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </div>
-  );
+    return (
+        <div className="product-page-container">
+            <MDBContainer>
+                <MDBRow>
+                    <MDBCol md="6">
+                        <ProductCarrousel product={product}/>
+                    </MDBCol>
+                    <MDBCol md="6">
+                        <ProductPageContent
+                            product={product}
+                            addOrderDetails={addOrderDetails}
+                        />
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </div>
+    );
 }
+
 export default Product;
