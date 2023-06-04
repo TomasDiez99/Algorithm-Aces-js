4f7e1c786defae2c531cbbe51253b295b19be88b
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-06-04 17:11:01 -0300

    refactor history page
    now show order details in each shopping cart zoom

--- la-gloria-store/src/pages/HistoryPage.js
+++ la-gloria-store/src/pages/HistoryPage.js
@@ -7,6 +7,7 @@ function HistoryPage() {
   const email = params.clientEmail;
   const [shoppingCarts, setShoppingCarts] = useState([]);
   const [expandedRows, setExpandedRows] = useState([]);
+  const [orderDetails, setOrderDetails] = useState([]);
 
   useEffect(() => {
     const fetchShoppingCarts = async () => {
@@ -24,11 +25,24 @@ function HistoryPage() {
     fetchShoppingCarts();
   }, [email]);
 
-  const handleRowClick = (index) => {
+  const fetchOrderDetails = async (shoppingCartId) => {
+    try {
+      const response = await fetch(
+        `https://la-gloria-store-algorithm-aces.vercel.app/rest/order-details/shopping-cart/${shoppingCartId}`
+      );
+      const data = await response.json();
+      setOrderDetails(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleRowClick = async (index, shoppingCartId) => {
     const rowIndex = expandedRows.indexOf(index);
     const newExpandedRows = [...expandedRows];
     if (rowIndex === -1) {
       newExpandedRows.push(index);
+      await fetchOrderDetails(shoppingCartId);
     } else {
       newExpandedRows.splice(rowIndex, 1);
     }
@@ -55,7 +69,10 @@ function HistoryPage() {
                   <td>{cart.date}</td>
                   <td>{cart.total_price}</td>
                   <td>
-                    <button className="btn btn-primary btn-sm" onClick={() => handleRowClick(index)}>
+                    <button
+                      className="btn btn-primary btn-sm"
+                      onClick={() => handleRowClick(index, cart.id)}
+                    >
                       {expandedRows.includes(index) ? "-" : "+"}
                     </button>
                   </td>
@@ -64,7 +81,28 @@ function HistoryPage() {
                   <tr>
                     <td colSpan="3">
                       <div className="order-details">
-                        {/*Insert order details*/}
+                        <table>
+                          <thead>
+                            <tr>
+                              <th>Product Amount</th>
+                              <th>Created At</th>
+                              <th>Updated At</th>
+                              <th>Shopping Cart ID</th>
+                              <th>Product ID</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {orderDetails.map((orderDetail) => (
+                              <tr key={orderDetail.id}>
+                                <td>{orderDetail.product_amount}</td>
+                                <td>{orderDetail.created_at}</td>
+                                <td>{orderDetail.updated_at}</td>
+                                <td>{orderDetail.shopping_cart_id}</td>
+                                <td>{orderDetail.product_id}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
                       </div>
                     </td>
                   </tr>
