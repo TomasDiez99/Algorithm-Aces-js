5a6558f57735744f1b640b8488df28188cfec1f2
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-06-20 14:41:58 -0300

    added loading image to home product cards

--- la-gloria-store/src/components/HomePageComponents/ProductCard.js
+++ la-gloria-store/src/components/HomePageComponents/ProductCard.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 function ProductCard(props) {
   const { id, name, image, price, enable, stock, getUpdatedStock } = props;
+  const [isLoading, setIsLoading] = useState(true);
+
+  function handleImageLoadError(e) {
+    e.target.src = "/notfound.png";
+  }
+
+  function handleImageLoad() {
+    setIsLoading(false);
+  }
 
   function isProductAvailable() {
     return enable && getUpdatedStock(id, stock) > 0;
@@ -10,13 +20,19 @@ function ProductCard(props) {
 
   return (
     <div className="card">
+      {isLoading && (
+        <img
+          src="loading.png"
+          className="loading-image"
+          alt="Loading"
+        />
+      )}
       <img
         src={image}
-        className="card-img-top"
+        className={`card-img-top ${isLoading ? "hidden" : ""}`}
         alt={name}
-        onError={(e) => {
-          e.target.src = "/notfound.png"; // Mostrar notfound.png si la carga de la imagen falla
-        }}
+        onLoad={handleImageLoad}
+        onError={handleImageLoadError}
       />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
