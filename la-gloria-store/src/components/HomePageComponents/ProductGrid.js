30b921fc0eca7034ea7d585055a571822cf977fa
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-06-20 15:31:45 -0300

    added tooltip to next and prev buttons in home

--- la-gloria-store/src/components/HomePageComponents/ProductGrid.js
+++ la-gloria-store/src/components/HomePageComponents/ProductGrid.js
@@ -1,90 +1,96 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
-import '../../styles/home.css';
-import {useNavigate} from "react-router-dom";
+import "../../styles/home.css";
+import { useNavigate } from "react-router-dom";
 
 function ProductGrid(props) {
-    const {categoryFilter, brandFilter, getUpdatedStock} = props;
-    const [currentPage, setCurrentPage] = useState(1);
-    const [lastPage, setLastPage] = useState(1);
-    const [products, setProducts] = useState([]);
-    const navigate = useNavigate();
+  const { categoryFilter, brandFilter, getUpdatedStock } = props;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        let url;
-        if (categoryFilter !== "" && brandFilter !== "") {
-            url = `https://la-gloria-store-algorithm-aces.vercel.app/rest/products/category/${categoryFilter}/brand/${brandFilter}?page=${currentPage}`;
-        } else if (categoryFilter !== "") {
-            url = `https://la-gloria-store-algorithm-aces.vercel.app/rest/products/category/${categoryFilter}?page=${currentPage}`;
-        } else if (brandFilter !== "") {
-            url = `https://la-gloria-store-algorithm-aces.vercel.app/rest/products/brand/${brandFilter}?page=${currentPage}`;
+  useEffect(() => {
+    let url;
+    if (categoryFilter !== "" && brandFilter !== "") {
+      url = `https://la-gloria-store-algorithm-aces.vercel.app/rest/products/category/${categoryFilter}/brand/${brandFilter}?page=${currentPage}`;
+    } else if (categoryFilter !== "") {
+      url = `https://la-gloria-store-algorithm-aces.vercel.app/rest/products/category/${categoryFilter}?page=${currentPage}`;
+    } else if (brandFilter !== "") {
+      url = `https://la-gloria-store-algorithm-aces.vercel.app/rest/products/brand/${brandFilter}?page=${currentPage}`;
+    } else {
+      url = `https://la-gloria-store-algorithm-aces.vercel.app/rest/products?page=${currentPage}`;
+    }
+    fetchProductsFromApi(url);
+  }, [categoryFilter, brandFilter, currentPage]);
+
+  const fetchProductsFromApi = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.data.length !== 0) {
+          setProducts(json.data);
+          setLastPage(json.meta.last_page);
+        } else if (currentPage !== 1) {
+          setCurrentPage(1);
         } else {
-            url = `https://la-gloria-store-algorithm-aces.vercel.app/rest/products?page=${currentPage}`;
+          alert(
+            "There are no products for the combination of filters selected"
+          );
         }
-        fetchProductsFromApi(url);
-    }, [categoryFilter, brandFilter, currentPage]);
+      })
+      .catch(() => {
+        navigate("/error");
+      });
+  };
 
-    const fetchProductsFromApi = (url) => {
-        fetch(url)
-            .then((response) => response.json())
-            .then((json) => {
-                if (json.data.length !== 0) {
-                    setProducts(json.data);
-                    setLastPage(json.meta.last_page);
-                } else if (currentPage !== 1) {
-                    setCurrentPage(1);
-                } else {
-                    alert(
-                        "There are no products for the combination of filters selected"
-                    );
-                }
-            })
-            .catch(() => {
-                navigate("/error");
-            });
-    };
+  const goToPage = (page) => {
+    if (page >= 1 && page <= lastPage) {
+      setCurrentPage(page);
+    }
+  };
 
-    const goToPage = (page) => {
-        if (page >= 1 && page <= lastPage) {
-            setCurrentPage(page);
-        }
-    };
-
-    return (
-        <div className="page-background-color radius-component">
-            <div className="grid-margin productGridStyle">
-                {products.map((product) => (
-                    <div className="cardContainerStyle" key={product.id}>
-                        <ProductCard
-                        id={product.id}
-                        name={product.name}
-                        image={product.image}
-                        price={product.price}
-                        enable={product.enable}
-                        stock={product.stock}
-                        getUpdatedStock={getUpdatedStock} />
-
-                    </div>
-                ))}
-            </div>
-            <div className="paginateButtonStyle">
-                <button
-                    className="btn btn-red"
-                    onClick={() => goToPage(currentPage - 1)}
-                    disabled={currentPage === 1}
-                >
-                    Previous
-                </button>
-                <button
-                    className="btn btn-red"
-                    onClick={() => goToPage(currentPage + 1)}
-                    disabled={currentPage === lastPage}
-                >
-                    Next
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="page-background-color radius-component">
+      <div className="grid-margin productGridStyle">
+        {products.map((product) => (
+          <div className="cardContainerStyle" key={product.id}>
+            <ProductCard
+              id={product.id}
+              name={product.name}
+              image={product.image}
+              price={product.price}
+              enable={product.enable}
+              stock={product.stock}
+              getUpdatedStock={getUpdatedStock}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="paginateButtonStyle">
+        <button
+          className="btn btn-red"
+          onClick={() => goToPage(currentPage - 1)}
+          disabled={currentPage === 1}
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Previous Page"
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-red"
+          onClick={() => goToPage(currentPage + 1)}
+          disabled={currentPage === lastPage}
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Next Page"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ProductGrid;
