46b40c85f80b5611304c14ca5906e849c8104900
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-06-20 14:28:35 -0300

    added image not found to invalid product images

--- la-gloria-store/src/components/HomePageComponents/ProductCard.js
+++ la-gloria-store/src/components/HomePageComponents/ProductCard.js
@@ -1,33 +1,40 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 function ProductCard(props) {
-    const { id, name, image, price, enable, stock, getUpdatedStock } = props;
+  const { id, name, image, price, enable, stock, getUpdatedStock } = props;
 
-    function isProductAvailable() {
-        return enable && getUpdatedStock(id, stock) > 0;
-    }
+  function isProductAvailable() {
+    return enable && getUpdatedStock(id, stock) > 0;
+  }
 
-    return (
-        <div className="card">
-            <img src={image} className="card-img-top" alt={name}/>
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <p className="card-text">${price}</p>
-                <div className="text-center">
-                    {isProductAvailable() ? (
-                        <Link to={`/product/${id}`} className="btn btn-primary">
-                            See details
-                        </Link>
-                    ) : (
-                        <button className="btn btn-primary" disabled>
-                            Product Unavailable
-                        </button>
-                    )}
-                </div>
-            </div>
+  return (
+    <div className="card">
+      <img
+        src={image}
+        className="card-img-top"
+        alt={name}
+        onError={(e) => {
+          e.target.src = "/notfound.png"; // Mostrar notfound.png si la carga de la imagen falla
+        }}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">${price}</p>
+        <div className="text-center">
+          {isProductAvailable() ? (
+            <Link to={`/product/${id}`} className="btn btn-primary">
+              See details
+            </Link>
+          ) : (
+            <button className="btn btn-primary" disabled>
+              Product Unavailable
+            </button>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default ProductCard;
