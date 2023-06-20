554167df1566224feee06c8c8c8d8f1fa73b8f70
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-06-20 14:47:04 -0300

    added image not found to product page
    fixed image size in carrousel css

--- la-gloria-store/src/components/ProductPageComponents/ProductCarrousel.js
+++ la-gloria-store/src/components/ProductPageComponents/ProductCarrousel.js
@@ -1,29 +1,28 @@
 import React from "react";
-import {MDBCarousel, MDBCarouselInner, MDBCarouselItem} from "mdbreact";
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem } from "mdbreact";
 import "../../styles/product-page.css";
 
 function ProductCarrousel(props) {
-    const {product} = props;
+  const { product } = props;
 
-    return (
-        <div className="carousel-container">
-            <MDBCarousel activeItem={1} length={1} showControls showIndicators>
-                <MDBCarouselInner>
-                    <MDBCarouselItem itemId="1">
-                        <img
-                            className="carousel-image"
-                            src={
-                                product
-                                    ? product.image
-                                    : "/loading.png"
-                            }
-                            alt="Imagen 1"
-                        />
-                    </MDBCarouselItem>
-                </MDBCarouselInner>
-            </MDBCarousel>
-        </div>
-    );
+  return (
+    <div className="carousel-container">
+      <MDBCarousel activeItem={1} length={1} showControls showIndicators>
+        <MDBCarouselInner>
+          <MDBCarouselItem itemId="1">
+            <img
+              className="carousel-image"
+              src={product ? product.image : "/loading.png"}
+              alt="Imagen 1"
+              onError={(e) => {
+                e.target.src = "/notfound.png";
+              }}
+            />
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </div>
+  );
 }
 
 export default ProductCarrousel;
