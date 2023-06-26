0a270e86eea9d9a3c80aaa444c64d6d88145b1cf
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-06-26 19:32:22 -0300

    more buttons added and fixed history page

--- la-gloria-store/src/pages/ErrorPage.js
+++ la-gloria-store/src/pages/ErrorPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../errorPage.css';
+import '../styles/errorPage.css';
 
 const ErrorPage = () => {
   return (
     <div className="error-container">
       <h1 className='error-h1'>¡Ups! Something went wrong.</h1>
       <p className='error-p'>Sorry for the inconvenience. Please try to go back to the start and try again.</p>
-      <Link to="/" className="btn btn-primary error-btn">Home page</Link>
+      <Link to="/" className="btn error-btn">Home page</Link>
     </div>
   );
 }
