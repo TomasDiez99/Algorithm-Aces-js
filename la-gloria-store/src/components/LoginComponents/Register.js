afed170e7533e37dc53b0c589837c8c58da77797
AxelFontana <axelfontana01@gmail.com>
2023-12-09 11:33:04 -0300

    Implement client registration functionality

--- la-gloria-store/src/components/LoginComponents/Register.js
+++ la-gloria-store/src/components/LoginComponents/Register.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import "../../styles/global.css";
 import "../../styles/login.css";
+import {useNavigate} from "react-router-dom";
+
+const EMAIL_REGEX = /.+@.+\.[A-Za-z]+$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 function Register() {
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -14,11 +19,38 @@ function Register() {
         setPassword(e.target.value);
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Aquí puedes realizar acciones adicionales, como enviar la información a un servidor
-        console.log("Email:", email);
-        console.log("Password:", password);
+
+        const vEmail = EMAIL_REGEX.test(email);
+        const vPassword = PASSWORD_REGEX.test(password);
+        if(!vEmail || !vPassword){
+            console.log("email or password invalid")
+        }
+        else{
+            try {
+                const response = await fetch("http://127.0.0.1:8000/rest/auth/register", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({email, password}),
+                });
+
+                if (!response.ok) {
+                    throw new Error("error creating client");
+                }
+                else {
+                    console.log("client registered")
+                }
+
+                navigate("/");
+            } catch (error) {
+                console.error("Error during client creation:", error);
+                navigate("/error");
+            }
+        }
+
     };
 
     return (
