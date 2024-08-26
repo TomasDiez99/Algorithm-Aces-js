cbd590b697aefe30d90fd351c0debdebf1b26f32
Tomas Diez <dieztomasjoaquin@gmail.com>
2024-08-26 16:41:41 -0300

    correct handling error on retry

--- la-gloria-store/src/utils.js
+++ la-gloria-store/src/utils.js
@@ -3,37 +3,39 @@ function min(a, b) {
 }
 
 export async function performGet({url, attempts = 4}) {
-
     let response = undefined;
 
-    let requestInit = {}
-    requestInit.method = "GET";
-    requestInit.headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Referer": "https://algorithm-aces.vercel.app/",
+    const requestInit = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Referer": "https://algorithm-aces.vercel.app/",
+        }
     };
 
-
     for (let i = 0; i < min(attempts, 10); i++) {
-
-        // wait 1 second
+        // wait 200 milliseconds before each attempt
         await new Promise((resolve) => setTimeout(resolve, 200));
-        try {
 
+        try {
             response = await fetch(url, requestInit);
+
             if (response.ok) {
                 return response;
+            } else {
+                console.error(`Attempt ${i + 1}: Server responded with status ${response.status}`);
             }
         } catch (e) {
-            console.error(" Error fetching with {Get} method: on attempt ", i + 1, response.statusText, response);
+            console.error(`Attempt ${i + 1}: Error fetching with {GET} method`, e.message);
         }
     }
 
     if (response) {
-        console.error(" Error fetching with {Get} method: ", response.statusText, response);
+        console.error("Error fetching with {GET} method: ", response.statusText);
         return response.statusText;
+    } else {
+        console.error("Failed to fetch after multiple attempts.");
+        return "Fetch failed after multiple attempts.";
     }
-
-
 }
